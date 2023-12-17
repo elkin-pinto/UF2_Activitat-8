65fdbea189ed482016b2c58d83393ef861d73e24
elkin-david <elkin.pinto.7e7@itb.cat>
2023-12-17 11:15:59 +0100

    solo falta hacer la linea

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,32 +11,60 @@ data class pos (
 fun main() {
     val scan = Scanner(System.`in`)
     val tablero = crearTablero(scan)
-    val pieza = crearPieza(tablero)
-    mourePieza(pieza,tablero,scan)
+    do {
+        val pieza = crearPieza(tablero)
+        mourePieza(pieza,tablero,scan)
+        hacerLinea(tablero)
+    } while (!gameOver(tablero,pieza))
+    println("GAME OVER!!")
+}
+
+
+fun gameOver (tablero: Array<Array<Char>>, pieza: Array<pos>):Boolean {
+    for (y in 0..3) {
+        for (x in tablero[y].indices) if (tablero[y][x] == '1') return true
+    }
+    return false
 }
 
 fun colision(pieza: Array<pos>, tablero: Array<Array<Char>>,input:Char):Boolean {
     if (input == 'a' || input == 'A' || input == 'd' || input == 'D') {
-        var respuesta:Boolean
-        val bordes_y = pos(0,0)
+        val bordes_y = pos(tablero[0].lastIndex,0)
         for (i in pieza) {
-            if (i.y > bordes_y.y ) bordes_y.y = i.y
-            if (i.y < bordes_y.y) bordes_y.x = i.y
+            if (i.x > bordes_y.y ) bordes_y.y = i.x
+            if (i.x < bordes_y.x) bordes_y.x = i.x
+        }
+        if (bordes_y.y < (tablero[0].size - 1) && (input == 'd' || input == 'D')) return true
+        if (bordes_y.x > 0 && (input == 'a' || input == 'A')) return true
+    }
+
+
+    else if (input == 's' || input == 'S') {
+        var bordes_x = 0
+        for (i in pieza) {
+            if (i.y > bordes_x) bordes_x = i.y
+        }
+        if (bordes_x < tablero.size - 1) {
+            for (i in pieza) {
+                if (tablero[bordes_x + 1][i.x] == '1' && i.y == bordes_x) return false
+            }
+            return true
         }
     }
-    return true
+    return false
 }
 
 fun mostrarTablero (tablero:Array<Array<Char>>) {
 
-    println("Controles: D = derecha, A = izquierda, espcio = bajar (Solo cojera el primer caracter)")
-    for (x in 0 until tablero.size) {
-        for (y in 0 until tablero[x].size) {
-            print("${tablero[x][y]} ")
+    println("Controles: D = derecha, A = izquierda, S = bajar (Solo cojera el primer caracter)")
+    for (y in 0 until tablero.size) {
+        for (x in 0 until tablero[y].size) {
+            print("${tablero[y][x]} ")
         }
         println("")
     }
 }
+
 
 
 fun mourePieza(pieza: Array<pos>, tablero: Array<Array<Char>>,scan:Scanner) {
@@ -44,19 +72,28 @@ fun mourePieza(pieza: Array<pos>, tablero: Array<Array<Char>>,scan:Scanner) {
         for (i in 0..3) {
             for (e in 0 until tablero[i].size) tablero[i][e] = '-'
         }
-        for (i in pieza) {
-            tablero[i.x][i.y] = '1'
-        }
+        for (i in pieza) tablero[i.y][i.x] = '1'
         mostrarTablero(tablero)
         val input = scan.next()[0]
         if (colision(pieza,tablero,input)) {
             when(input) {
-                'a','A' -> {for (i in pieza) i.y -= 1}
-                'd','D' -> {for (i in pieza) i.y += 1}
+                'a','A' -> {for (i in pieza) i.x -= 1}
+                'd','D' -> {for (i in pieza) i.x += 1}
             }
         }
 
-    } while(input != ' ')
+    } while(input != 's')
+    val input = 's'
+    do {
+        var bajando = colision(pieza,tablero,input)
+        if (bajando) {
+            for (i in pieza) tablero[i.y][i.x] = '0'
+            for (i in 0..3) for (e in 0 until tablero[i].size) tablero[i][e] = '-'
+            for (i in pieza) i.y += 1
+            for (i in pieza) tablero[i.y][i.x] = '1'
+        }
+    } while (bajando)
+
 }
 
 fun crearPieza(tablero:Array<Array<Char>>):Array<pos> {
@@ -65,27 +102,27 @@ fun crearPieza(tablero:Array<Array<Char>>):Array<pos> {
     when (piezas[Math.floor(Math.random()*piezas.size).toInt()]) {
         "quadrado" -> {
             pieza[0] = pos(0,0)
-            pieza[1] = pos(0,1)
-            pieza[2] = pos(1,0)
+            pieza[1] = pos(1,0)
+            pieza[2] = pos(0,1)
             pieza[3] = pos(1,1)
         }
         "linea" -> {
             pieza[0] = pos(0,0)
-            pieza[1] = pos(1,0)
-            pieza[2] = pos(2,0)
-            pieza[3] = pos(3,0)
+            pieza[1] = pos(0,1)
+            pieza[2] = pos(0,2)
+            pieza[3] = pos(0,3)
         }
         "lInversa" -> {
-            pieza[0] = pos(0,1)
+            pieza[0] = pos(1,0)
             pieza[1] = pos(1,1)
-            pieza[2] = pos(2,1)
-            pieza[3] = pos(2,0)
+            pieza[2] = pos(1,2)
+            pieza[3] = pos(0,2)
         }
         "lAcostada" -> {
             pieza[0] = pos(0,0)
-            pieza[1] = pos(1,0)
+            pieza[1] = pos(0,1)
             pieza[2] = pos(1,1)
-            pieza[3] = pos(1,2)
+            pieza[3] = pos(2,1)
         }
     }
     return pieza
@@ -99,13 +136,13 @@ fun crearTablero(scan:Scanner): Array<Array<Char>> {
     var y:Int
     do {
         println("Di el tamaño horizontal: ")
-        x = comprobarInt("El valor introducido no es correcto",scan)
-        println("Di el tamaño vertical: ")
         y = comprobarInt("El valor introducido no es correcto",scan)
+        println("Di el tamaño vertical: ")
+        x = comprobarInt("El valor introducido no es correcto",scan)
         if (x < 4 || y < 4) println("Uno de los dos valores introducidos no cumple con las medidas minimas")
     }while (x < 4 || y < 4)
 
-    return Array(x+4) { Array(y){ '0' } }
+    return Array(y+4) { Array(x){ '0' } }
 }
 
 
@@ -127,3 +164,29 @@ fun comprobarInt(mensajeError: String,scan:Scanner):Int {
     return scan.nextInt()
 }
 
+fun hacerLinea(tablero: Array<Array<Char>>) {
+    var linea: Int = 0
+    var hayLinea: Boolean
+    for (y in tablero.lastIndex downTo 0) {
+        hayLinea = true
+        for (x in tablero[y]) {
+            linea = y
+            if (x != '1') hayLinea = false
+        }
+
+        if (hayLinea) {
+            for (i in tablero[linea].indices) tablero[linea][i] = '0'
+
+            for (y in linea downTo  4) for (x in tablero[y].indices) {
+                if (tablero[y][x] == '1') {
+                    var posY = y
+                    while (posY != tablero.lastIndex || tablero[posY + 1][x] == '1') {
+                        tablero[posY][x] = '0'
+                        tablero[posY + 1][x] = '1'
+                        posY += 1
+                    }
+                }
+            }
+        }
+    }
+}
